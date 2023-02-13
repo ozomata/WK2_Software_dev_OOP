d7fc7c8280b9cd2ae43d26ac6bf32b6c4f70e68f
Asun <ozomata@live.com>
2023-02-13 17:10:01 +0000

    corrected from revenue to profit

--- sales_prediction.py
+++ sales_prediction.py
@@ -6,13 +6,15 @@ class Sales:
         
     def profit(self, year):
         yearSales=self.amt+(self.amt*(int(year)-1)*0.05)
-        return float (yearSales+yearSales*0.23)
+        return float (yearSales*0.23)
     
    
     
 user_amt= float(input("input the sales: "))
-user_year= input("input the  year of  sales: ")
-
+#user_year= input("input the  year of  sales: ")
+user_year= input("input the  Number of years you you want reported: ")
 userSales = Sales (user_amt, user_year)
 
-print("The profit for year", userSales.year , " : " ,   userSales.profit(int(user_year)))
+#print("The profit for year", userSales.year , " : " ,   userSales.profit(int(user_year)))
+for i in range(int(user_year)):
+    print("The profit for year",i+1, " : " ,   userSales.profit(i))
